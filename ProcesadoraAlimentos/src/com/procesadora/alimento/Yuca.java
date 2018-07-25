380e21b8a7b2e06de304fab1e37871a02a8cbb80
Daniela <dgarciaj@ucenfotec.ac.cr>
2018-07-25 11:12:45 -0600

    Este es el patron de builder

---
+++ ProcesadoraAlimentos/src/com/procesadora/alimento/Yuca.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.procesadora.alimento;
+
+/**
+ *
+ * @author Mario
+ */
+public class Yuca extends Alimento{
+
+    public Yuca(){
+        this.nombre = "Yuca";
+    }
+    
+    public double costo() {
+        return 500.00;
+    }
+    
+}
---
+++ ProcesadoraAlimentos/src/com/procesadora/alimento/extras/DecoradorAlimento.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.procesadora.alimento.extras;
+
+import com.procesadora.alimento.Alimento;
+
+/**
+ *
+ * @author Mario
+ */
+public abstract class  DecoradorAlimento extends Alimento{
+    @Override
+    public abstract String getNombre();
+}
